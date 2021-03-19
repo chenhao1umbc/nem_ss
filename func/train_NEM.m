5bb769c1c8fc96d801a150fd0bcda46536fadb72
chenhao1umbc <chenhao1@umbc.edu>
2021-03-19 11:32:35 -0400

    adding functions

---
+++ func/train_NEM.m
@@ -0,0 +1,2 @@
+function [vj, cj, Rj, neural_net] = train_NEM(x, v, model, opts)
+% This function is the main body of the algorithm
---
+++ pre_data.m
@@ -0,0 +1,12 @@
+%This file will generate real(not complex) toy data
+% Experiment 1
+% Suppose there are 3 classes, the vj is given, 6 Chennels with real "steer vector"
+% vj shape of 50*50, the component sources have variation of AOA and power 
+% all the x(n,f) the stft is real number
+% 1. try Duang's method
+% 2. try neural EM with 3 networks
+
+% Experiment 2
+% Suppose there are 3 classes, AOA and power are given
+% there are variation of vj in each class
+% try neural EM with 3 networks to see how \gamma_j works
