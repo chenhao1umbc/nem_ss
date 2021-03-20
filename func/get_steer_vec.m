2c047b821fdedf3652cf6f9655550a42f4a84349
chenhao1umbc <chenhao1@umbc.edu>
2021-03-20 12:28:55 -0400

    still working on toy experiment 1

--- func/get_steer_vec.m
+++ func/get_steer_vec.m
@@ -1,10 +1,11 @@
 function steer_vec = get_steer_vec(aoa, n_channel)
 eps = 1e-30;
-elementPos = (0:.1:.5);
+elementPos = (0:.1:n_channel*0.1-0.1);
 c = physconst('LightSpeed');
 fc = 1e9;
 lam = c/fc;
 ang = [aoa;0];
 sv = steervec(elementPos/lam,ang);
 steer_vec = reshape(real(sv),[1, n_channel]) + eps;
+steer_vec = steer_vec/ norm(steer_vec);
 end
--- pre_data.m
+++ pre_data.m
@@ -13,15 +13,18 @@
 clear
 clc
 close all
+
+addpath('func')
+rng(0)
 visualize = 0;
 
 %% load the original data
-importfile('/home/chenhao1/Matlab/nem_ss/vj1.png');
+importfile('/home/chenhao1/Matlab/nem_ss/data/vj1.png');
 vj1 = mean(cdata, 3);
 vj1(vj1 == 0) = 3;
-importfile('/home/chenhao1/Matlab/nem_ss/vj2.png');
+importfile('/home/chenhao1/Matlab/nem_ss/data/vj2.png');
 vj2 = mean(cdata, 3);
-importfile('/home/chenhao1/Matlab/nem_ss/vj3.png')
+importfile('/home/chenhao1/Matlab/nem_ss/data/vj3.png')
 vj3 = mean(cdata, 3);
 vj{1} = vj1/norm(vj1);
 vj{2} = vj2/norm(vj2);
@@ -39,6 +42,7 @@ for j = 1:3
     end
 end
 
+% for each source the vj(PSD) is normalized to 1
 vj = temp; % for later calc., vj will be shape of [50, 50, 3]
 if visualize
     figure;
@@ -46,25 +50,62 @@ if visualize
 end
 
 %% Experiment 1 data
-% vj(n,f) = sum(|x(n,f).*steer_vec|.^2)/n_channel, where x(n,f) is the real number
-n_channel = 6;
+% real world data is complex time series cj(t)
+% it has n_channel channels as cj(t).*steer_vec
+% vj(n,f) = sum(|STFT(cj(t).*steer_vec)|.^2)/n_channel
+
+% Here to make sure cj(n,f) is real number, is real number, we have
+% cj(n,f) = cj_(n,f).*steer_vec
+% vj(n,f) = sum(|cj(n,f)|.^2)/n_channel, where cj(n,f) is the real number
+n_channel = 3;
 J = 3; % how many sources
 
 aoa = 45; % in degrees
 steer_vec = get_steer_vec(aoa, n_channel);
-x = zeros(50*50, n_channel, J);
+cjnf = zeros(50*50, n_channel, J); % [N*F, n_channel, n_sources]
 for j = 1:J
     temp = vj(:,:,j);
-    x(:, :, j) = ((temp(:)*n_channel).^0.5)./steer_vec;
+    cj_nf = (temp(:)./(steer_vec.*steer_vec)).^0.5; %x >=0
+    cjnf(:, :, j) = cj_nf.* sign(rand(50*50, n_channel)-0.5).*steer_vec;
 end
 
-% check if vj
+% check if vj can be calculated from cjnf
 for j = 1:3
-a{j} = abs(x(:,:,j).*steer_vec).^2;
+v = reshape(sum(abs(cjnf(:,:,j)).^2/n_channel, 2), [50, 50]);
+fprintf('check the difference between generated vj and origianl one')
+difference = sum(abs(v -vj(:, :, j)) , 'all')  % should be 0
 end
-v1 = reshape(sum(a{1}, 2)/n_channel, [50, 50]);
-sum(abs(v1 -vj(:, :, 1)) , 'all')  % should be 0
 
+% adding power diff
+max_db = 20;
+power_db = rand(1, 3)*max_db; % power diff for each source
+for j = 1:3
+    cjnf(:,:,j) = 10^(power_db(j)/20) * cjnf(:, :, j);
+end
+
+xnf = sum(cjnf, 3); % sum over all the sources, shape of [N*F, n_channel]
+if visualize  % to see the mixture data
+    for i = 1:n_channel
+        figure;
+        imagesc(reshape(xnf(:, i), [50, 50]))
+        title(['Channel', num2str(i)])
+        colorbar
+    end
+end
+
+% code Generate online data with diff powers and various ang 
+load('./data/vj.mat')
+n_channel = 3;
+J = 3; % how many sources
+
+aoa = rand(n_channel)*180; % in degrees
+steer_vec = get_steer_vec(aoa, n_channel);
+cjnf = zeros(50*50, n_channel, J); % [N*F, n_channel, n_sources]
+for j = 1:J
+    temp = vj(:,:,j);
+    cj_nf = (temp(:)./(steer_vec.*steer_vec)).^0.5; %x >=0
+    cjnf(:, :, j) = cj_nf.* sign(rand(50*50, n_channel)-0.5).*steer_vec;
+end
 
 %% Experiment 2 data
 
