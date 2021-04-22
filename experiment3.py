e2712238fe4e4ad96300e37acd6179f7d1c9ca4a
chenhao1umbc <chenhao1@umbc.edu>
2021-04-22 18:01:13 -0400

    working on experiment 3

--- experiment3.py
+++ experiment3.py
@@ -3,8 +3,15 @@
 """only generate 1-class vj and let it move. Also train only one neural network to see if it could capture
 the patter
 """
-#%%
 from utils import *
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+#%% load options for training 
+opts = {}
+opts['n_epochs'] = 100  
+opts['lr'] = 0.01
+opts['n_batch'] = 64  # batch size
+opts['d_gamma'] = 16 # gamma dimesion 16*16 to 200*200
 
 # %% load data
 from PIL import Image  # install Pillow
@@ -22,12 +29,35 @@ for r in range(105):
         if n < N:
             data[n] = torch.roll(d, (r, c), (0, 1))
             n += 1
-gamma = torch.rand(N, 16, 16)
+gamma = torch.rand(N, opts['d_gamma'], opts['d_gamma'])
+" wrap as data loader"
+data = Data.TensorDataset(gamma, data)
+tr = Data.DataLoader(data, batch_size=opts['n_batch'], shuffle=True, drop_last=True)
 
-#%% load options for training
-opts = {}
-opts['n_epochs'] = 100  
-opts['lr'] = 0.01
-opts['n_batch'] = 64  # batch size
-opts['d_gamma'] = gamma.shape[-1] # gamma dimesion 16*16 to 200*200
-opts['n_s'] = 1  # number of sources
+#%% set neural networks
+model = UNetHalf(n_channels=1, n_classes=1).cuda()
+optimizer = optim.RAdam(model.parameters(),
+                lr= opts['lr'],
+                betas=(0.9, 0.999),
+                eps=1e-8,
+                weight_decay=0)
+
+optim_gamma = torch.optim.SGD([gamma], lr= opts['lr'])
+
+for epoch in range(opts['n_epochs']):    
+    for i, (gamma, v) in enumerate(tr): # gamma [n_batch, n_f, n_t]
+        out = model(gamma[:,None].cuda())
+        loss = ((out - v[:,None].cuda())**2).sum()/opts['n_batch']
+
+        optimizer.zero_grad()   
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=500)
+        optimizer.step()
+        torch.cuda.empty_cache()
+    
+    if epoch%1 ==0:
+        plt.figure()
+        plt.semilogy(loss_cv, '--xr')
+        plt.title('val loss per epoch')
+        plt.show()
+
