37d14a5f47c6597b4720a5cf8e2a26bdd19ed092
chenhao1umbc <chenhao1@umbc.edu>
2021-05-06 15:10:19 -0400

    EM from matlab is done, torch.float32 is not accurate enough for matrix inversion. Nan come from it too.

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ torch.set_printoptions(linewidth=160)
 torch.set_default_dtype(torch.float64)
 
 "make the result reproducible"
-# torch.manual_seed(0)
+torch.manual_seed(1)
 torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
 print('done loading')
@@ -39,13 +39,15 @@ def calc_ll_real2(x, vhat, Rj, Rb):
     Rcj = Rcj.reshape(N, F, M, M)
     Rx = Rcj + Rb 
     Rx = (Rx + Rx.transpose(-1, -2))/2
-    l = -0.5*(2*np.pi*Rx.det()).log() - (x[..., None, :] @ Rx.inverse() @ x[..., None]).squeeze()
+    Rx_inv = Rx.inverse()
+    l = -0.5*(2*np.pi*Rx.det()).log() - (x[..., None, :] @ Rx_inv @ x[..., None]).squeeze()
 
     return l.sum()
 
 "reproduce the Matlab result"
 d = sio.loadmat('data/x1M5.mat')
-x, c = torch.tensor(d['x']), torch.tensor(d['c'])
+x, c = torch.tensor(d['x'], dtype=torch.get_default_dtype()), \
+    torch.tensor(d['c'], dtype=torch.get_default_dtype())
 M, N, F, J = c.shape
 NF = N*F
 x = x.permute(1,2,0)  # shape of [N, F, M]
@@ -53,7 +55,7 @@ c = c.permute(1,2,3,0) # shape of [N, F, J, M]
 
 "loade data"
 d = sio.loadmat('data/v.mat')
-vj = torch.tensor(d['v'])
+vj = torch.tensor(d['v'], dtype=torch.get_default_dtype())
 pwr = torch.ones(1, 3)  # signal powers
 max_iter = 400
 
@@ -65,14 +67,14 @@ Rxxhat = (x[...,None] @ x[..., None, :]).sum((0,1))/NF
 Rj = torch.zeros(J, M, M)
 ll_traj = []
 
-for iter in range(max_iter):
+for i in range(max_iter):
     "E-step"
     Rs = vhat.diag_embed()
     Rx = Hhat @ Rs @ Hhat.t() + Rb
     W = Rs @ Hhat.t() @ Rx.inverse()
     shat = W @ x[...,None]
     Rsshatnf = shat @ shat.transpose(-1,-2) + Rs - W@Hhat@Rs
-    
+
     Rsshat = Rsshatnf.sum([0,1])/NF
     Rxshat = (x[..., None] @ shat.transpose(-1,-2)).sum((0,1))/NF
 
@@ -80,21 +82,17 @@ for iter in range(max_iter):
     vhat = Rsshatnf.diagonal(dim1=-1, dim2=-2)
     Hhat = Rxshat @ Rsshat.inverse()
     Rb = Rxxhat - Hhat@Rxshat.t() - Rxshat@Hhat.t() + Hhat@Rsshat@Hhat.t()
-
+    
     "compute log-likelyhood"
     for j in range(J):
         Rj[j] = Hhat[:, j][..., None] @ Hhat[:, j][..., None].t()
-    # if torch.tensor(calc_ll_real2(x, vhat, Rj, Rb).item()).isnan() : 
-    #     input('nan happened')
     ll_traj.append(calc_ll_real2(x, vhat, Rj, Rb).item())
     
-
-    if iter%50 == 0:
+    if i%50 == 0:
         plt.figure(100)
         plt.plot(ll_traj,'o-')
         plt.show()
         
-print('how many nan: ', torch.tensor(ll_traj).isnan().sum().item())
 "display results"
 for j in range(J):
     plt.figure(j)
@@ -106,7 +104,10 @@ for j in range(J):
     plt.imshow(vj[:,:,j])
     plt.title('Ground-truth')
     plt.colorbar()
+    plt.show()
 
+# pytorch inverse stability issue
+print('how many nan: ', torch.tensor(ll_traj).isnan().sum().item())
 
 
 #%% Neural EM algorithm
