6cea3d4bdad16c236ff753819be85b9f2daa56a7
chenhao1umbc <chenhao1@umbc.edu>
2021-05-06 17:09:40 -0400

    working on 1 sample EM complex

--- rank1_complex.py
+++ rank1_complex.py
@@ -28,7 +28,7 @@ print('done loading')
 
 # %% EM algorithm for one complex sample
 
-def calc_ll_real2(x, vhat, Rj, Rb):
+def calc_ll_cpx2(x, vhat, Rj, Rb):
     """Rj shape of [J, M, M]
         vhat shape of [N, F, J]
         Rb shape of [M, M]
@@ -39,16 +39,15 @@ def calc_ll_real2(x, vhat, Rj, Rb):
     Rcj = vhat.reshape(N*F, J) @ Rj.reshape(J, M*M)
     Rcj = Rcj.reshape(N, F, M, M)
     Rx = Rcj + Rb 
-    Rx = (Rx + Rx.transpose(-1, -2))/2
-    Rx_inv = Rx.inverse()
-    l = -0.5*(2*np.pi*Rx.det()).log() - (x[..., None, :] @ Rx_inv @ x[..., None]).squeeze()
+    Rx = (Rx + Rx.transpose(-1, -2).conj())/2
+    l = -(np.pi*Rx.det()).log() - (x[..., None, :]@Rx.inverse()@x[..., None]).squeeze()
 
     return l.sum()
 
 "reproduce the Matlab result"
-d = sio.loadmat('data/x1M3.mat')
-x, c = torch.tensor(d['x'], dtype=torch.get_default_dtype()), \
-    torch.tensor(d['c'], dtype=torch.get_default_dtype())
+d = sio.loadmat('data/x1M3_cpx.mat')
+x, c = torch.tensor(d['x'], dtype=torch.cdouble), \
+    torch.tensor(d['c'], dtype=torch.cdouble)
 M, N, F, J = c.shape
 NF = N*F
 x = x.permute(1,2,0)  # shape of [N, F, M]
@@ -56,38 +55,39 @@ c = c.permute(1,2,3,0) # shape of [N, F, J, M]
 
 "loade data"
 d = sio.loadmat('data/v.mat')
-vj = torch.tensor(d['v'], dtype=torch.get_default_dtype())
+vj = torch.tensor(d['v'], dtype=torch.cdouble)
 pwr = torch.ones(1, 3)  # signal powers
 max_iter = 400
 
 "initial"
-vhat = torch.randn(N, F, J).abs()
-Rb = torch.eye(M)
-Hhat = torch.randn(M, J)
+vhat = torch.randn(N, F, J).abs().to(torch.cdouble)
+Rb = torch.eye(M).to(torch.cdouble)
+Hhat = torch.randn(M, J).to(torch.cdouble)
 Rxxhat = (x[...,None] @ x[..., None, :]).sum((0,1))/NF
-Rj = torch.zeros(J, M, M)
+Rj = torch.zeros(J, M, M).to(torch.cdouble)
 ll_traj = []
 
 for i in range(max_iter):
     "E-step"
     Rs = vhat.diag_embed()
-    Rx = Hhat @ Rs @ Hhat.t() + Rb
-    W = Rs @ Hhat.t() @ Rx.inverse()
+    Rx = Hhat @ Rs @ Hhat.t().conj() + Rb
+    W = Rs @ Hhat.t().conj() @ Rx.inverse()
     shat = W @ x[...,None]
-    Rsshatnf = shat @ shat.transpose(-1,-2) + Rs - W@Hhat@Rs
+    Rsshatnf = shat @ shat.transpose(-1,-2).conj() + Rs - W@Hhat@Rs
 
     Rsshat = Rsshatnf.sum([0,1])/NF
-    Rxshat = (x[..., None] @ shat.transpose(-1,-2)).sum((0,1))/NF
+    Rxshat = (x[..., None] @ shat.transpose(-1,-2).conj()).sum((0,1))/NF
 
     "M-step"
     vhat = Rsshatnf.diagonal(dim1=-1, dim2=-2)
     Hhat = Rxshat @ Rsshat.inverse()
-    Rb = Rxxhat - Hhat@Rxshat.t() - Rxshat@Hhat.t() + Hhat@Rsshat@Hhat.t()
+    Rb = Rxxhat - Hhat@Rxshat.t().conj() - \
+        Rxshat@Hhat.t().conj() + Hhat@Rsshat@Hhat.t().conj()
     
     "compute log-likelyhood"
     for j in range(J):
-        Rj[j] = Hhat[:, j][..., None] @ Hhat[:, j][..., None].t()
-    ll_traj.append(calc_ll_real2(x, vhat, Rj, Rb).item())
+        Rj[j] = Hhat[:, j][..., None] @ Hhat[:, j][..., None].t().conj()
+    ll_traj.append(calc_ll_cpx2(x, vhat, Rj, Rb).item())
     
     if i%50 == 0:
         plt.figure(100)
