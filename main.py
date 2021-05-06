9ec2aaf2d711a51c8ade089e78dc396f99a1fbeb
chenhao1umbc <chenhao1@umbc.edu>
2021-05-06 09:08:19 -0400

    add data folder not sync .mat files

--- main.py
+++ main.py
@@ -24,23 +24,24 @@ torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
 print('done loading')
 
-#%% loade data
-d = sio.loadmat('v2.mat')
-[N,F,J] = d['v'].shape
-M = 5          # no of channels
-pwr = torch.ones(1, 3)  # signal powers
-max_iter = 400
-nvar = 1e-6    # noise variance
 
 
 # %% EM  algorithm for one sample
-# reproduce the Matlab result
-d = sio.loadmat('x1M3.mat')
-x, c = d['x'], d['c']
+"reproduce the Matlab result"
+d = sio.loadmat('x1M5.mat')
+x, c = torch.tensor(d['x'], dtype=torch.float), torch.tensor(d['c'], dtype=torch.float)
+M, N, F, J = c.shape
+#%% loade data
+d = sio.loadmat('v2.mat')
+vj = torch.tensor(d['v'], dtype=torch.float)
+pwr = torch.ones(1, 3)  # signal powers
+max_iter = 400
+
 
 
 #%% Neural EM algorithm
-data = h5py.File('x5000M5.mat', 'r')
-x = torch.tensor(data['x'], dtype=torch.float) # [sample, N, F, channel]
-xtr, xcv, xte = x[:4000], x[4000:4500], x[4500:]
+
+# data = h5py.File('x5000M5.mat', 'r')
+# x = torch.tensor(data['x'], dtype=torch.float) # [sample, N, F, channel]
+# xtr, xcv, xte = x[:4000], x[4000:4500], x[4500:]
 
