5ad646c6685db500c4281410ba7108ee66b2056d
HC <chenhao1@umbc.edu>
2021-05-22 12:23:00 -0400

    regenerate data, make sure the aoa is 30 degree apart

--- gen_data.m
+++ gen_data.m
@@ -10,7 +10,28 @@ pwr = [1 1 1];  % signal powers
 nvar = 1e-6;    % noise variance
 I = 3000; % how many traning samples
 
-%%
+%% with no shift
+vp = zeros(N,F,J);
+for j = 1:J
+    vp(:,:,j) = pwr(j)*v(:,:,j);
+end
+
+x = zeros(I,M,N,F);
+for i = 1:I     
+    theta = [rand(1)*30, rand(1)*30+60, -rand(1)*30-30]*pi/180;  % len=M, signal AOAs  
+    h = exp(-1i*pi*(0:M-1)'*sin(theta));
+    s = zeros(N,F,J);
+    c = zeros(M,N,F,J);
+    for j = 1:J
+        s(:,:,j) = (randn(N,F)+1i*randn(N,F))/sqrt(2).*sqrt(vp(:,:,j));
+        for m = 1:M
+            c(m,:,:,j) = h(m,j)*s(:,:,j);
+        end
+    end
+    x(i,:,:,:) = sum(c,4) + (randn(M,N,F)+1i*randn(M,N,F))/sqrt(2)*sqrt(nvar);   % M x N x F
+end
+
+%% with shift
 vp = zeros(N,F,J);
 for j = 1:J
     vp(:,:,j) = pwr(j)*v(:,:,j);
@@ -44,7 +65,7 @@ for i = 1:I
     temp = randperm(l_n3y);
     vs(:,:,3) = circshift(vs(:,:,3), n3y(temp(1)), 1); % shift x
     
-    theta = (rand(1,M)*300 -150)*pi/180;  % signal AOAs  
+    theta = [rand(1)*30, rand(1)*30+60, -rand(1)*30-30]*pi/180;  % len=M, signal AOAs  
     h = exp(-1i*pi*(0:M-1)'*sin(theta));
     s = zeros(N,F,J);
     c = zeros(M,N,F,J);
